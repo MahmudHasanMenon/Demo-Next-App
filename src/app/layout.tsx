49b688f24977c088f9bc083736ca64e2a83b08b0
menon <menon@menons-MacBook-Pro.local>
2024-06-30 22:14:07 -0600

    public preview issue fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,20 +17,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content="C-Tribe Passport" />
-        <meta property="og:description" content="C-Tribe Passport: access to knowledge, social and financial capital during the C-Tribe Festival and Conference" />
-        <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/project-brain-f7594.appspot.com/o/ctribe-preview-image.png?alt=media&token=6483586d-04db-4fa1-be3e-0da8282493a2" />
-        <meta property="og:image:width" content="650" />
-        <meta property="og:image:height" content="300" />
-        <meta property="og:url" content="https://ctribe-event-web-staging.vercel.app/" />
-        <meta property="og:type" content="website" />
+        <title>
+          University of Alberta
+        </title>
 
-        <meta name="twitter:card" content="C-Tribe Passport" />
-        <meta name="twitter:title" content="C-Tribe Passport" />
-        <meta name="twitter:description" content="C-Tribe Passport: access to knowledge, social and financial capital during the C-Tribe Festival and Conference" />
-        <meta name="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/project-brain-f7594.appspot.com/o/ctribe-preview-image.png?alt=media&token=6483586d-04db-4fa1-be3e-0da8282493a2" />
-        <meta name="twitter:image:alt" content="C-Tribe Passport" />
+
+        <meta name="description" content="The University of Alberta is a Top 5 Canadian university located in Edmonton, Alberta, and home to 40,000 students in a wide variety of programs." />
+
+
+        <meta property="og:url" content="https://www.ualberta.ca/index.html" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content=" University of Alberta" />
+        <meta property="og:description" content="The University of Alberta is a Top 5 Canadian university located in Edmonton, Alberta, and home to 40,000 students in a wide variety of programs." />
+        <meta property="og:image" content="https://www.ualberta.ca/media-library/ualberta/homepage/university-of-alberta-logo.jpg" />
+
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="ualberta.ca" />
+        <meta property="twitter:url" content="https://www.ualberta.ca/index.html" />
+        <meta name="twitter:title" content="University of Alberta" />
+        <meta name="twitter:description" content="The University of Alberta is a Top 5 Canadian university located in Edmonton, Alberta, and home to 40,000 students in a wide variety of programs." />
+        <meta name="twitter:image" content="https://www.ualberta.ca/media-library/ualberta/homepage/university-of-alberta-logo.jpg" />
+
       </head>
       <body className={inter.className}>{children}</body>
     </html>
