428fa7a8c30b7b9708c8b2e64700fab00d5e3a72
menon <menon@menons-MacBook-Pro.local>
2024-07-01 00:01:51 -0600

    copy from ctribe project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,13 +17,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>
           C-Tribe Passport
         </title>
 
-
         <meta name="description" content="C-Tribe Passport: access to knowledge, social and financial capital during the C-Tribe Festival and Conference." />
-
 
         <meta property="og:url" content="https://ctribe-event-web-staging.vercel.app" />
         <meta property="og:type" content="website" />
@@ -36,7 +35,7 @@ export default function RootLayout({
         <meta property="twitter:domain" content="ctribe-event-web-staging.vercel.app" />
         <meta property="twitter:url" content="https://ctribe-event-web-staging.vercel.app" />
         <meta name="twitter:title" content="C-Tribe Passport" />
-        <meta name="twitter:description" content="C-Tribe Passport: access to knowledge, social and financial capital during the C-Tribe Festival and Conference." />
+        <meta name="twitter:description" content="C-Tribe Passport: access to knowledge, social and financial capital during the C-Tribe Festival and Conference" />
         <meta name="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/project-brain-f7594.appspot.com/o/ctribe-preview-image.png?alt=media&token=6483586d-04db-4fa1-be3e-0da8282493a2" />
 
       </head>
